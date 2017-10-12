5dcda406a52e17ffc365f002175cc97dd1354bd2
wehb12 <32541936+wehb12@users.noreply.github.com>
2017-10-12 10:31:54 +0100

    12/10/17 - Morning
    Added while loop, do user can now enter as many guesses as they like. Need to add a check to see if the battleship is sunk.

--- programming for games/arrays/battleships.cpp
+++ programming for games/arrays/battleships.cpp
@@ -20,7 +20,7 @@ char** init(const int size)
 
 void printField(char** field, const int size)
 {
-	cout << ' ' << ' ' << ' ' << 'A' << ' ' << 'B' << ' ' << 'C' << ' ' << 'D' << ' ' << 'E' << endl;
+	cout << ' ' << ' ' << ' ' << '0' << ' ' << '1' << ' ' << '2' << ' ' << '3' << ' ' << '4' << endl;
 	for (int i = 0; i < size; i++)
 	{
 		cout << i << "  ";
@@ -41,28 +41,42 @@ int main()
 	field[1][2] = '1';
 	field[1][3] = '1';
 	field[1][4] = '1';
-	
-	cout << "Your Guesses:" << endl << endl;
-	printField(guess, size);
 
-	int x, y;
-	cout << endl << "Please enter an X coord: ";
-	cin >> x;
-	cout << endl << "Please enter an Y coord: ";
-	cin >> y;
-
-	if (field[x][y] == 1)
+	while (1)
 	{
-		guess[x][y] = 'X';
-		cout << endl << "HIT!" << endl << endl;
-	}
-	else
-	{
-		guess[x][y] = 'O';
-		cout << endl << "MISS!" << endl << endl;
-	}
+		cout << "Your Guesses:" << endl << endl;
+		printField(guess, size);
 
-	printField(guess, size);
+		int x = 0, y = 0;
+		cout << endl << "Please enter an X coord: ";
+		cin >> x;
+		cout << endl << "Please enter an Y coord: ";
+		cin >> y;
+
+		if (field[y][x] == '1')
+		{
+			guess[y][x] = 'X';
+			cout << endl << "HIT!" << endl;
+		}
+		else
+		{
+			guess[y][x] = 'O';
+			cout << endl << "MISS!" << endl;
+		}
+		
+		char cont = NULL;
+		while (!((cont == 'Y') || (cont == 'y')))
+		{
+			cout << endl << "Continue? (Y/N)... ";
+			cin >> cont;
+
+			if ((cont == 'N') || (cont == 'n'))
+				return 0;
+			else if (!((cont == 'Y') || (cont == 'y')))
+				cout << endl << "Please enter 'Y' or 'N'... ";
+		}
+		system("CLS");
+	}
 
     return 0;
 }
