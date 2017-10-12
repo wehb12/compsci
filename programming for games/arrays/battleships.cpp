86b84776047eb0848bc5560ccb12248588920f5c
wehb12 <32541936+wehb12@users.noreply.github.com>
2017-10-12 14:49:10 +0100

    battleships.cpp -12/10/17
    Added a class and extra functionality - the program now checks to see if you've sunk the battleship.

--- programming for games/arrays/battleships.cpp
+++ programming for games/arrays/battleships.cpp
@@ -6,46 +6,98 @@
 
 using namespace std;
 
-char** init(const int size)
+class Battleships
 {
-	char** field = new char*[size];
+public:
+	Battleships();
+	~Battleships();
+	void PrintGuesses();
+	void RegisterGuess(int x, int y);
+	bool CheckScore();
+private:
+	const int size = 5;
+	char **field;
+	char **guess;
+	int hitPoints;
+	int* score;
+};
+
+Battleships::Battleships()
+{
+	field = new char*[size];
+	guess = new char*[size];
 	for (int i = 0; i < size; i++)
 	{
 		field[i] = new char[size];
-		memset(field[i], '0', size * sizeof(bool));
+		guess[i] = new char[size];
+		memset(field[i], '0', size * sizeof(char));
+		memset(guess[i], '_', size * sizeof(char));
 	}
-
-	return field;
-}
-
-void printField(char** field, const int size)
-{
-	cout << ' ' << ' ' << ' ' << '0' << ' ' << '1' << ' ' << '2' << ' ' << '3' << ' ' << '4' << endl;
-	for (int i = 0; i < size; i++)
-	{
-		cout << i << "  ";
-		for (int j = 0; j < size; j++)
-			cout << field[i][j] << ' ';
-		cout << endl;
-	}
-}
-
-int main()
-{
-	const int size = 5;
-
-	char** field = init(size);
-	char** guess = init(size);
 
 	field[1][1] = '1';
 	field[1][2] = '1';
 	field[1][3] = '1';
 	field[1][4] = '1';
 
+	hitPoints = 4;
+
+	score = new int;
+	*score = 0;
+}
+
+Battleships::~Battleships()
+{
+	for (int i = 0; i < size; i++)
+	{
+		delete[] field[i];
+		delete[] guess[i];
+	}
+	delete field;
+	delete guess;
+}
+
+void Battleships::PrintGuesses()
+{
+	cout << ' ' << ' ' << ' ' << '0' << ' ' << '1' << ' ' << '2' << ' ' << '3' << ' ' << '4' << endl;
+	for (int i = 0; i < size; i++)
+	{
+		cout << i << "  ";
+		for (int j = 0; j < size; j++)
+			cout << guess[i][j] << ' ';
+		cout << endl;
+	}
+}
+
+void Battleships::RegisterGuess(int x, int y)
+{
+	if (field[y][x] == '1')
+	{
+		guess[y][x] = 'X';
+		*score = *score + 1;
+		cout << endl << "HIT!" << endl;
+	}
+	else
+	{
+		guess[y][x] = 'O';
+		cout << endl << "MISS!" << endl;
+	}
+}
+
+bool Battleships::CheckScore()
+{
+	if (*score == hitPoints)
+		return true;
+	else return false;
+}
+
+int main()
+{
+	Battleships game1;
+
 	while (1)
 	{
 		cout << "Your Guesses:" << endl << endl;
-		printField(guess, size);
+		game1.PrintGuesses();
 
 		int x = 0, y = 0;
 		cout << endl << "Please enter an X coord: ";
@@ -53,15 +105,12 @@ int main()
 		cout << endl << "Please enter an Y coord: ";
 		cin >> y;
 
-		if (field[y][x] == '1')
+		game1.RegisterGuess(x, y);
+		if (game1.CheckScore())
 		{
-			guess[y][x] = 'X';
-			cout << endl << "HIT!" << endl;
-		}
-		else
-		{
-			guess[y][x] = 'O';
-			cout << endl << "MISS!" << endl;
+			cout << endl << "You sunk my battleship!!" << endl;
+			cout << "END OF GAME";
+			return 0;
 		}
 		
 		char cont = NULL;
@@ -77,6 +126,4 @@ int main()
 		}
 		system("CLS");
 	}
-
-    return 0;
 }
