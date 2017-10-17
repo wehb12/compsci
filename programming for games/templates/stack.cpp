29c9a3f2381512de17653ec862c8f8cfc9665762
wehb12 <32541936+wehb12@users.noreply.github.com>
2017-10-17 10:05:48 +0100

    stack.cpp 17/10/17
    stack.push(data) wasn't working because I was making a larger array but then forgetting to set the last element of the array to the data input. Works now.
    
    stack.push(data) is very inefficient. It must copy the entire stack into a new area of memory that is one element larger.
    
    Very fast to read and pop(), but not fast to push. pop() function doesn't delete the memory location of the top element, it just decrements the 'length' field.

--- programming for games/templates/stack.cpp
+++ programming for games/templates/stack.cpp
@@ -8,41 +8,39 @@ template <typename T>
 class Stack
 {
 protected:
-    int length;
-    T* elem;
+	int length;
+	T* elem;
 public:
-    inline Stack() : length(0) { elem = new T; }
-    ~Stack();
-    virtual void push(T data);// = 0;
-    void pop();
-    inline T top() { return *(elem + length - 1); }
-    inline int size() { return size; }
-    bool empty();
+	inline Stack() : length(0) { }
+	~Stack();
+	virtual void push(T data);// = 0;
+	void pop();
+	inline T top() { return *(elem + length - 1); }
+	inline int size() { return size; }
+	bool empty();
 };
 
 template <typename T>
 Stack<T>::~Stack()
 {
-    if (length != 0)
-        delete[] elem;
-    
-    elem = NULL;
+	if (length != 0)
+		delete[] elem;
+
+	elem = NULL;
 }
 
 template <typename T>
 void Stack<T>::pop()
 {
-    --length;
-    
-    delete (elem + length);
+	--length;
 }
 
 template <typename T>
 bool Stack<T>::empty()
 {
-    if (length == 0)
-        return true;
-    else return false;
+	if (length == 0)
+		return true;
+	else return false;
 }
 
 //template <typename T>
@@ -56,38 +54,48 @@ bool Stack<T>::empty()
 template <typename T>
 void Stack<T>::push(T data)
 {
-    ++length;
-    
-    if (length == 1)
-    {
-        *elem = data;
-        return;
-    }
-        
-    T* buff = elem;
-    
-    delete[] elem;
-    
-    elem = new T[length];
-    
-    for (int i = length - 2; i >= 0; --i)
-        *(elem + i) = *(buff + i);
-    
-    *(elem + length - 1) = *(buff + length - 1);
+	++length;
+
+	if (length == 1)
+	{
+		elem = new T;
+		*elem = data;
+		return;
+	}
+
+	T* buff = new T[length - 1];
+
+	for (int i = 0; i < length - 1; ++i)
+		*(buff + i) = *(elem + i);
+
+	delete[] elem;
+	elem = NULL;
+
+	elem = new T[length];
+
+	for (int i = length - 2; i >= 0; --i)
+		*(elem + i) = *(buff + i);
+
+	*(elem + length - 1) = data;
+
+	delete[] buff;
+	buff = NULL;
 }
 
 int main()
 {
-    Stack<int> stack;
-    
-    stack.push(2);
-    stack.push(1);
-    stack.push(3);
-    stack.push(1);
-    
-    while (!stack.empty())
-    {
-        cout << stack.top() << endl;
-        stack.pop();
-    }
+	Stack<int> stack;
+
+	stack.push(2);
+	stack.push(1);
+	stack.push(3);
+	stack.push(1);
+
+	while (!stack.empty())
+	{
+		cout << stack.top() << endl;
+		stack.pop();
+	}
+
+	return 0;
 }
