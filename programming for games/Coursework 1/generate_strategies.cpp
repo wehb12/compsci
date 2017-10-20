dbf9c68172b2a0338ccedf203bd8802a00a0427b
wehb12 <32541936+wehb12@users.noreply.github.com>
2017-10-20 17:17:51 +0100

    generate_strategies - 20/10/17
    Added in a vector of char vectors, that store essentially bool data types (but vector<bool> doesn't work). These are flags which tell the script whether a variable has been printed so that it isn't printed twice in a line. In hindsight, it needn't be a vector of vectors, just a 1D array will do. You don't need to store this info for previous lines, only the current line.

--- programming for games/Coursework 1/generate_strategies.cpp
+++ programming for games/Coursework 1/generate_strategies.cpp
@@ -10,12 +10,14 @@
 
 using namespace std;
 
+int PsuedoRand(int, float&);
+
 class Strategy
 {
 public:
-	Strategy(const string txt);
-	inline Strategy() : name("default") { strat = new vector<vector<string>>;  }
-	inline Strategy(const Strategy& cpy) : name("default") { strat = new vector<vector<string>>; }
+	inline Strategy(const string txt) : name(txt) { strat = new vector<vector<string>>(1); };
+	inline Strategy() : name("default") { strat = new vector<vector<string>>(1);  }
+	inline Strategy(const Strategy& cpy) : name("default") { strat = new vector<vector<string>>(1); }
 	~Strategy() { delete strat; }
 	friend ostream& operator<<(ostream& ostr, const Strategy& str);
 	inline string GetName() { return name; }
@@ -23,11 +25,6 @@ protected:
 	string name;
 	vector<vector<string>>* strat;
 };
-
-Strategy::Strategy(const string txt) : name(txt)
-{
-	strat = new vector<vector<string>>(1);
-}
 
 ostream& operator<<(ostream& ostr, const Strategy& str)
 {
@@ -44,14 +41,65 @@ ostream& operator<<(ostream& ostr, const Strategy& str)
 class CreateStrategy : public Strategy
 {
 public:
-	inline CreateStrategy(const string txt) : Strategy(txt) { }
-	inline CreateStrategy() : Strategy() { }
-	inline CreateStrategy(const CreateStrategy& cpy) : Strategy(cpy) { }
-	~CreateStrategy() { }
+	inline CreateStrategy(const string txt) : Strategy(txt) { FirstChecksLine(); }
+	inline CreateStrategy() : Strategy() { FirstChecksLine(); }
+	inline CreateStrategy(const CreateStrategy& cpy) : Strategy(cpy) { FirstChecksLine();  }
+	~CreateStrategy() { delete checks; }
 	inline void AddFeature(string txt) { strat->back().push_back(txt); }
 	void AddFeature(int num);
 	void NewLine();
+	void SetFlag(int flagNum);
+	bool GetFlag(int flagNum);
+protected:
+	// boolean checks to ensure a strategy that makes sense is constructed, with no variable repeats
+	// values ares set to true if their variable has been printed before
+	// [0] - ALLOUTCOMES_W, [1] - ALLOUTCOMES_X, [2] - ALLOUTCOMES_Y, [3] - ALLOUTCOMES_Z,
+	// [4] - INTEGER, [5] - ITERATIONS, [6] - MYSCORE, [7] - =, < OR >
+	// [8] - Only one variable left, so if =, < OR > hasn't been, then print one
+	// [9] - All variables used, so terminate line
+	vector<vector<char>>* checks;
+	void FirstChecksLine();
+	void NewChecksLine();
+	void ComputeSum();
 };
+
+void CreateStrategy::SetFlag(int flagNum)
+{
+	checks->back()[flagNum] = true;
+	ComputeSum();
+}
+
+bool CreateStrategy::GetFlag(int flagNum)
+{
+	return checks->back()[flagNum];
+}
+
+void CreateStrategy::ComputeSum()
+{
+	int sum = 0;
+	for (int i = 0; i < 5; ++i)
+		if (checks->back()[i])
+			++sum;
+	if (sum == 4)		// change these numbers when you add in flags [5] and [6]
+		checks->back()[8] = true;
+	else if (sum == 5)
+		checks->back()[9] = true;
+}
+
+void CreateStrategy::FirstChecksLine()
+{
+	checks = new vector<vector<char>>(0);
+	NewChecksLine();
+}
+
+void CreateStrategy::NewChecksLine()
+{
+	vector<char> temp;
+	checks->push_back(temp);
+
+	for (int i = 0; i < 10; ++i)
+		checks->back().push_back(false);
+}
 
 void CreateStrategy::AddFeature(int num)
 {
@@ -63,19 +111,19 @@ void CreateStrategy::NewLine()
 {
 	vector<string> temp(0);
 	strat->push_back(temp);
+	NewChecksLine();
 }
 
 int main()
 {
 	int strats = 0;			// number of startegies created
-	float length = 0.5;		// number that dictates length - not length in lines, probability of it being longer
-	const int lengthAdjust = 1;	// the higher this number, the lower the number of lines
-	int line = 1;			// which line are we on?
+	float length = 0.05;	// number that dictates length - not length in lines, probability of it being longer
+	int line = 1;			// which line of the strategy are we on?
 	int seed = 0;
 
 	++strats;
 
-	seed = 100;//rand() % 500;
+	seed = 247;//rand() % 500;
 	if (seed == 0)
 		seed = 1;
 
@@ -85,16 +133,12 @@ int main()
 
 	CreateStrategy strat1(id);
 
-	while (line < (((float)seed * length) / lengthAdjust))
+	while (line < ((float)seed * length))
 	{
 		strat1.AddFeature(line);
 
 		float exp = line;
-		float maxExp = log(2100000000) / log(seed);
-		while (exp > maxExp)
-			exp -= maxExp;
-
-		int randNum = pow(seed, exp);
+		int randNum = PsuedoRand(seed, exp);
 		randNum = randNum % 72;
 
 		if (randNum >= 36)	// print an exit condition
@@ -126,61 +170,82 @@ int main()
 			else
 			{
 				bool exitFlag = false;
-				bool secondHalf = false;
+				bool symbol = false;	// tells program a symbol is wanted next
 				int tempNum = randNum;
 
 				while (!exitFlag)
 				{
-					float maxExp = log(2100000000) / log(tempNum);
-					while ((exp > maxExp) && tempNum)
-						exp -= maxExp;
-
-					int temp = pow(tempNum, exp);
+					int temp = PsuedoRand(tempNum, exp);
 					temp = temp % 200;
 
 					if (tempNum <= 6)
-						strat1.AddFeature(temp);
+					{
+						if (!strat1.GetFlag(4))
+						{
+							strat1.AddFeature(temp);
+							strat1.SetFlag(4);
+							symbol = true;
+						}
+					}
 					else
 					{
-						if (tempNum <= 14)
+						if ((tempNum <= 14) && !strat1.GetFlag(0))
+						{
 							strat1.AddFeature("ALLOUTCOMES_W");
-						else if (tempNum <= 17)
+							strat1.SetFlag(0);
+							symbol = true;
+						}
+						else if ((tempNum <= 17) && !strat1.GetFlag(1))
+						{
 							strat1.AddFeature("ALLOUTCOMES_X");
-						else if (tempNum <= 20)
+							strat1.SetFlag(1);
+							symbol = true;
+						}
+						else if ((tempNum <= 20) && !strat1.GetFlag(2))
+						{
 							strat1.AddFeature("ALLOUTCOMES_Y");
-						else
+							strat1.SetFlag(2);
+							symbol = true;
+						}
+						else if (!strat1.GetFlag(3))
+						{
 							strat1.AddFeature("ALLOUTCOMES_Z");
+							strat1.SetFlag(3);
+							symbol = true;
+						}
 					}
 
-					if (temp >= 80)
+					if (symbol || (!strat1.GetFlag(7) && strat1.GetFlag(8)) || strat1.GetFlag(9))
 					{
-						if (!secondHalf)
+						if ((temp >= 40) || (!strat1.GetFlag(7) && strat1.GetFlag(8)) || strat1.GetFlag(9))
 						{
-							if (temp <= 119)
-								strat1.AddFeature("=");
-							else if (temp <= 159)
-								strat1.AddFeature("<");
+							if (!strat1.GetFlag(7))
+							{
+								if (temp <= 119)
+									strat1.AddFeature("=");
+								else if (temp <= 159)
+									strat1.AddFeature("<");
+								else
+									strat1.AddFeature(">");
+								strat1.SetFlag(7);
+							}
 							else
-								strat1.AddFeature(">");
-							secondHalf = true;
+								exitFlag = true;
 						}
 						else
-							exitFlag = true;
-					}
-					else
-					{
-						if (temp <= 39)
-							strat1.AddFeature("+");
-						else
-							strat1.AddFeature("-");
+						{
+							if (temp <= 19)
+								strat1.AddFeature("+");
+							else
+								strat1.AddFeature("-");
+						}
+						symbol = false;
 					}
 
-					exp++;
 					tempNum++;
-					maxExp = log(2100000000) / log(tempNum);
-					while ((exp > maxExp) && tempNum)
-						exp -= maxExp;
-					tempNum = pow(tempNum, exp);
+					exp++;
+
+					tempNum = PsuedoRand(tempNum, exp);
 					tempNum = tempNum % 24;
 				}
 			}
@@ -188,13 +253,14 @@ int main()
 			strat1.AddFeature("GOTO");
 			strat1.AddFeature(line + 2);
 			strat1.NewLine();
-			if (line >= (((float)seed * length) / lengthAdjust) - 1)
+			if (line >= ((float)seed * length) - 1)
 			{
 				for (int i = 0; i < 2; ++i)
 				{
 					randNum = pow(seed, line);
 					randNum = randNum % 36;
 
+					strat1.AddFeature(line);
 					if (randNum <= 11)
 						strat1.AddFeature("BETRAY");
 					else if (randNum <= 23)
@@ -215,4 +281,15 @@ int main()
 	file << strat1;
 
     return 0;
+}
+
+int PsuedoRand(int base, float& exp)
+{
+	if (base == 0)
+		base = 1;
+	float maxExp = log(2100000000) / log(base);
+	while (exp > abs(maxExp))
+		exp -= maxExp;
+
+	return (int)pow(base, exp);
 }
