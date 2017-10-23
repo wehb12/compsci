834186fac998ab3933a266bb8f73515ce4ed7b52
wehb12 <32541936+wehb12@users.noreply.github.com>
2017-10-23 14:06:18 +0100

    read_strategies.cpp - 23/10/17 14:06

--- programming for games/Coursework 1/read_strategies.cpp
+++ programming for games/Coursework 1/read_strategies.cpp
@@ -2,22 +2,80 @@
 // ---------------------------------------------------------------- //
 // Reads and executes strategies                                    //
 
+#include "stdafx.h"
 #include "strategy.h"
+#include "prisoner.h"
 
 using namespace std;
 
+void Game(Prisoner&, Prisoner&);
+
 int main()
 {
-	int strats = 1;
+	string id1 = "strat";
+	id1 += to_string(1);
+	id1 += ".txt";
 
-	string id = "Desktop/Uni/Masters/Programming_for_Games/Coursework_1/strat";
-	id += to_string(strats);
-	id += ".txt";
+	string id2 = "strat";
+	id2 += to_string(2);
+	id2 += ".txt";
 
-	ReadStrategy strat(id);
+	ReadStrategy strat1(id1);
+	if (strat1.OpenFile())
+	{
+		Prisoner A(strat1);
+		ReadStrategy strat2(id2);
+		if (strat2.OpenFile())
+		{
+			Prisoner B(strat2);
 
-	string out = "Desktop/Uni/Masters/Programming_for_Games/Coursework_1/read_strat.txt";
+			for (int i = 0; i < 5; ++i)
+				Game(A, B);
+		}
+	}
 
-	ofstream output(out);
-	output << strat;
+	return 0;
+}
+
+void Game(Prisoner& A, Prisoner& B)
+{
+	int AOutcome = A.Run();
+	int BOutcome = B.Run();
+
+	if (AOutcome != -1)
+	{
+		if (BOutcome != -1)
+		{
+			if (AOutcome == 0)
+			{
+				if (BOutcome == 0)
+				{
+					A.RegisterOutcome('Z');
+					B.RegisterOutcome('Z');
+				}
+				else
+				{
+					A.RegisterOutcome('Y');
+					B.RegisterOutcome('X');
+				}
+			}
+			else
+			{
+				if (BOutcome == 0)
+				{
+					A.RegisterOutcome('X');
+					B.RegisterOutcome('Y');
+				}
+				else
+				{
+					A.RegisterOutcome('W');
+					B.RegisterOutcome('W');
+				}
+			}
+		}
+		else
+			cout << "Error in prisoner B strategy" << endl;
+	}
+	else
+		cout << "Error in prisoner A strategy" << endl;
 }
