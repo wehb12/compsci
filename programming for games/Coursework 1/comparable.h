eff68c3ea019d256f39066a70c413d31fb7ecffe
wehb12 <32541936+wehb12@users.noreply.github.com>
2017-10-27 03:57:05 +0100

    comparable.h final

--- programming for games/Coursework 1/comparable.h
+++ programming for games/Coursework 1/comparable.h
@@ -1,6 +1,7 @@
 // Will Hinds, Computer Games Engineering MSc - comparable.h //
 // --------------------------------------------------------- //
 // Contains class definitions for comparable objects         //
+// Student ID: 170740805, Date: 27/10/17 10:00               //
 
 #pragma once
 
@@ -8,12 +9,15 @@
 
 using namespace std;
 
+// pure abstract Comparable class to allow for comparison of objects stored in BST
 class Comparable
 {
 public:
 	virtual int CompareTo(const Comparable& rhs) = 0;
 };
 
+
+// Integer Comparable used tot store line numbers
 class Integer : public Comparable
 {
 public:
